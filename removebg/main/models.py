9368f10089efe5056108a6b1858ec050774502b6
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-02-17 14:21:02 +0530

    Update models.py

--- removebg/main/models.py
+++ removebg/main/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 
 class Remove(models.Model):
-    Original_img = models.ImageField(upload_to="E:\Projects\Liftoff\Remove.bg\Remove.bg\removebg\Static\Uploads\Original")
-    Final_img = models.ImageField(upload_to="E:\Projects\Liftoff\Remove.bg\Remove.bg\removebg\Static\Uploads\Final",blank=True,null=True)
+    Original_img = models.ImageField(upload_to="\Uploads\Original")
+    Final_img = models.ImageField(upload_to="\Uploads\Final",blank=True,null=True)
     created_at = models.DateTimeField(auto_now_add=True)  
 
     def __str__(self):
