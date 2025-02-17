cbbcbf8b7d82a41070f7fa6b8df73a04e0f06eac
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-02-17 20:06:15 +0530

    Update views.py

--- removebg/main/views.py
+++ removebg/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def upload_pg(request):
+    return render(request,'upload.html')
+
+def result_pg(request):
+    return render(request,'result.html')
