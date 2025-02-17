4f47dda966df246619506a251f157bcbecd59cc9
Aashin C Anil <148208390+Aashin20@users.noreply.github.com>
2025-02-17 14:15:26 +0530

    Create Remove model

--- removebg/main/models.py
+++ removebg/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Remove(models.Model):
+    Original_img = models.ImageField(upload_to="E:\Projects\Liftoff\Remove.bg\Remove.bg\removebg\Static\Uploads\Original")
+    Final_img = models.ImageField(upload_to="E:\Projects\Liftoff\Remove.bg\Remove.bg\removebg\Static\Uploads\Final",blank=True,null=True)
+    created_at = models.DateTimeField(auto_now_add=True)  
+
+    def __str__(self):
+        return f"Image {self.id} - {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}"
